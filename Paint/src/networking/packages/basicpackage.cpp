ec36bea7d0692f5ece4fc741401b1ac1bdb02d62
smay1613 <dimaafa0@gmail.com>
2018-12-27 01:39:32 +0200

    Reworks basic package
    BasicPackage can now work in three modes: W, R, WR.
    Fixes issues with reading from datastream.

--- Paint/src/networking/packages/basicpackage.cpp
+++ Paint/src/networking/packages/basicpackage.cpp
@@ -1,15 +1,21 @@
 #include "basicpackage.h"
+#include <QDebug>
 
-BasicPackage::BasicPackage(networking::PType type)
-    : m_stream {&m_rawData, QIODevice::WriteOnly},
-      m_type {type}
+BasicPackage::BasicPackage(networking::PType type, const QIODevice::OpenModeFlag openMode)
+    : m_translationStream {&m_rawData, QIODevice::WriteOnly},
+      m_type {type},
+      m_mode {openMode}
 {
+    Q_ASSERT_X((openMode == QIODevice::OpenModeFlag::WriteOnly
+                || openMode == QIODevice::OpenModeFlag::ReadOnly
+                || openMode == QIODevice::OpenModeFlag::ReadWrite), "BasicPackage::BasicPackage(type, openMode)",
+                                                                    "INVALID MODE PROVIDED!");
 }
 
 BasicPackage::BasicPackage(const QVariant &data, networking::PType type)
-    : BasicPackage(type)
+    : BasicPackage(type, QIODevice::OpenModeFlag::WriteOnly)
 {
-    m_stream << static_cast<qint32>(m_type) << data;
+    m_translationStream << static_cast<qint32>(m_type) << data;
 }
 
 networking::PType BasicPackage::type() const
@@ -19,23 +25,31 @@ networking::PType BasicPackage::type() const
 
 QDataStream& operator>>(QDataStream& stream, BasicPackage& package)
 {
-    quint32 type;
-    stream >> type;
-    package.m_type = static_cast<networking::PType>(type);
+    if (package.m_mode != QIODevice::OpenModeFlag::WriteOnly) {
+        quint32 type;
+        stream >> type;
+        package.m_type = static_cast<networking::PType>(type);
 
-    QVariant data;
-    stream >> data;
-    package.m_rawData = data.toByteArray();
+        QVariant data;
+        stream >> data;
+        package.m_data = data;
+
+        if (package.m_mode == QIODevice::OpenModeFlag::ReadWrite) {
+            package.m_translationStream << static_cast<qint32>(type) << data;
+        }
+    }
 
     return stream;
 }
 
 QVariant BasicPackage::data() const
 {
-    return QVariant {m_rawData};
+    return m_mode == QIODevice::OpenModeFlag::WriteOnly ? QVariant {}
+                                                        : m_data;
 }
 
 QByteArray BasicPackage::rawData() const
 {
-    return m_rawData;
+    return m_mode == QIODevice::OpenModeFlag::ReadOnly ? QByteArray {}
+                                                        : m_rawData;
 }
--- Paint/src/networking/packages/basicpackage.h
+++ Paint/src/networking/packages/basicpackage.h
@@ -6,6 +6,8 @@
 /*
  * This class describes the protocol for transmitting data via network.
  *
+ * You can only use ReadingOnly, WriteOnly and WriteRead modes.
+ *
  * WRITING USAGE:
  * First, user must pack his data intro QVariant (e.g QVariant {<data>})
  * And create BasicPackage object with setting the data and the package type.
@@ -13,36 +15,45 @@
  * {qint32 type, QVariant data}.
  * After that, user can exchange this package with socket's write function by
  * calling rawData method, that will return raw data content.
+ * Mode will be automatically switched to WriteOnly.
  *
  * READING USAGE:
- * Default package should be created first, and then it could be filled with
- * datastream. operator >> will firstly extract the package type from the datastream
- * and set it to the object m_type property (that can be used by type getter)
- * and then it will extract QVariant data and fill she raw data (conversion QVariant->ByteArray).
- * Package data can be used by data() method that will create new QVariant based on raw data.
+ * Default package should be created first (NOTE: there is no sense to set the WriteOnly flag in case of
+ * default creating, because it cannot be filled later) and then it could be filled with datastream.
+ * Operator >> will save all data in members.
  *
  * NOTE: Data placed in QVariant must provide Datastream << and >> operator!
 */
 
+//TODO: MAKE COMMENTS DOXYGEN STYLE
 class BasicPackage : public IPackage
 {
 public:
-    BasicPackage(networking::PType type = networking::PType::INVALID);
+    BasicPackage(networking::PType type = networking::PType::INVALID,
+                     const QIODevice::OpenModeFlag openMode = QIODevice::OpenModeFlag::ReadOnly);
+
     BasicPackage(const QVariant& data, networking::PType type = networking::PType::INVALID);
     virtual ~BasicPackage() = default;
 
     networking::PType type() const override;
+
+    // Return empty QVariant if mode is WriteOnly
     QVariant data() const override;
 
+    // Returns empty bytearray if mode is ReadingOnly.
     QByteArray rawData() const override;
 
+    // Will do nothing in case if mode is WriteOnly.
     friend QDataStream& operator>> (QDataStream& stream, BasicPackage& package);
 
 protected:
     QByteArray m_rawData;
-    QDataStream m_stream;
+    QDataStream m_translationStream;
 
     networking::PType m_type;
+    QVariant m_data;
+
+    QIODevice::OpenModeFlag m_mode;
 };
 
 #endif // BASICPACKAGE_H
